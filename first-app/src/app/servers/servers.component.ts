d2f872645c7b1aa6c16553fa9a6678c3d2d4248e
victor.karetko <victor.karetko@octopod.com>
2017-11-13 00:36:16 +0300

    init commit

---
+++ first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  showStatus = true;
+  logs = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  switchShowStatus() {
+    this.logs.push(this.logs.length + 1);
+    this.showStatus = this.showStatus != true;
+  }
+}
