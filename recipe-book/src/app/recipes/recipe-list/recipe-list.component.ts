8e8157aec1fdf49438318a5ef01d0c011cc9695f
victor.karetko <victor.karetko@octopod.com>
2017-11-19 00:18:10 +0300

    added recipe model

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Test', 'http://vipest-okna.ru/prefix/414714.jpg'),
+    new Recipe('A Test Recipe 2', 'Test 2', 'https://im0-tub-ru.yandex.net/i?id=eb01083d56c652e18c772db3b062ebeb&n=13')
+  ];
 
   constructor() { }
 
