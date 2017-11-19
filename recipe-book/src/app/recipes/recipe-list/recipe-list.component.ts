094b1ef6966ba40d226eba9edeb8282382df4319
V1toss <vitoss@inbox.ru>
2017-11-19 23:57:37 +0300

    added ingredients to shopping list component

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Test', 'http://vipest-okna.ru/prefix/414714.jpg'),
+    new Recipe('A Test Recipe', 'Test', 'http://mealblog.ru/wp-content/uploads/2013/03/%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0-%D0%B8%D0%B7-%D1%80%D1%8B%D0%B1%D1%8B.jpg'),
     new Recipe('A Test Recipe 2', 'Test 2', 'https://im0-tub-ru.yandex.net/i?id=eb01083d56c652e18c772db3b062ebeb&n=13')
   ];
 
