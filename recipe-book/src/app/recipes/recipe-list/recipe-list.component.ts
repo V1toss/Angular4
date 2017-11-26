b0a86be5e411d4da6c2031957c5b94b225b0a302
V1toss <vitoss@inbox.ru>
2017-11-26 19:16:50 +0300

    added selecting items in items-list

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Test', 'http://mealblog.ru/wp-content/uploads/2013/03/%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0-%D0%B8%D0%B7-%D1%80%D1%8B%D0%B1%D1%8B.jpg'),
     new Recipe('A Test Recipe 2', 'Test 2', 'https://im0-tub-ru.yandex.net/i?id=eb01083d56c652e18c772db3b062ebeb&n=13')
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
